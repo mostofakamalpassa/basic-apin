c1dcdc9b3ec138d1c6eba6e050a0b7afea3adb56
mostofa kamal <mostofakamal1990@gmail.com>
2022-02-21 20:50:02 +0600

    basic javascript object idea for JSON format or string conversion

--- js/app.js
+++ js/app.js
@@ -1,4 +1,27 @@
-// const user = {id:1, name:'kamal', age:55}
+ const user = {id:1, name:'kamal', age:55}
 
 // console.log("API");
 // console.log(user)
+
+const stringifyUser = JSON.stringify(user);
+
+// console.log(stringifyUser);
+// console.log(user);
+
+const shop = {
+    name:'kamal shop',
+    adress:'Gazipur',
+    products:['apple','orange','blue','yellow'],
+    isExpensive:false,
+    profit:20000,
+    owner:{name:'jamal', profession:'abcd', salary:5000}
+};
+
+
+//console.log(shop);
+const shopString = JSON.stringify(shop);
+console.log(shopString);
+
+const convertToParse = JSON.parse(shopString);
+
+console.log(convertToParse);
